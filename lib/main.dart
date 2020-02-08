cf1c6382165a623dee3ee97487ac24a0b547beea
Osama <osamamuhanna68@gmail.com>
2020-02-09 00:53:58 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'utilities.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CarPooling',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Map());
+  }
+}
+
+class Map extends StatefulWidget {
+  Map({Key key}) : super(key: key);
+
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  GoogleMapController mapController;
+  static const _initialPosition = LatLng(24.636727, 46.694251);
+  LatLng _lastPosition = _initialPosition;
+  final Set<Marker> _markers = {};
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
+          initialCameraPosition:
+              CameraPosition(target: _initialPosition, zoom: 10),
+          onMapCreated: onCreated,
+          myLocationEnabled: true,
+          //mapToolbarEnabled: true,
+          compassEnabled: true,
+          markers: _markers,
+          onCameraMove: _onCameraMove,
+        ),
+        Positioned(
+          top: 40,
+          right: 10,
+          child: FloatingActionButton(
+            onPressed: _onAddMarkerPressed,
+            tooltip: "Add Marker",
+            backgroundColor: yellow,
+            child: Icon(
+              Icons.add_location,
+              color: white,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  void onCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    setState(() {
+      _lastPosition = position.target;
+    });
+  }
+
+  void _onAddMarkerPressed() {
+    setState(() {
+      _markers.add(Marker(markerId: MarkerId(_lastPosition.toString()),
+      position: _lastPosition,
+      infoWindow: InfoWindow(
+        title: "remember",
+        snippet: "good place"
+      ),
+      icon: BitmapDescriptor.defaultMarker
+      ));
+    });
+  }
+}
---
+++ lib/utilities.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const Color yellow = Colors.yellow;
+const Color white = Colors.white;
