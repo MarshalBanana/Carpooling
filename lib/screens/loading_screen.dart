78558acb086c52b2b8e89384d707f903951cc605
NNKamel <noureldinashraf6@gmail.com>
2020-04-23 21:38:53 +0300

    added signup before main home screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,26 @@
+import 'package:carpooling/utilities/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 
 import '../state/app_states.dart';
 import 'organizing_screen.dart';
+import '../screens/signup.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  AuthService auth;
+
+  @override
+  void initState() {
+    super.initState();
+    auth = AuthService();
+  }
+
   @override
   Widget build(BuildContext context) {
     final appState = Provider.of<AppState>(context);
@@ -37,6 +52,13 @@ class LoadingScreen extends StatelessWidget {
                     ),
                   ],
                 ))
-            : OrganizingScreen(title: 'CarPooling'));
+            : isSignedIn());
+  }
+
+  isSignedIn() {
+    if (auth.fUser != null) {
+      return OrganizingScreen(title: 'Carpooling');
+    } else
+      return SignUpScreen();
   }
 }
